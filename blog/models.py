6270c4760059f5a1e759b12611f51a6b2e500f88
Egor Ivanov <rust_lang_lover@proton.me>
2025-08-20 17:54:23 +0300

    ..

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
 
-# Create your models here.
+
+class SimpleContentBlock(blocks.StructBlock):
+    title = blocks.CharBlock(max_length=100, help_text="Заголовок блока")
+    content = blocks.RichTextBlock(features=['bold', 'italic', 'link'])
+    image = ImageChooserBlock(required=False)
+    alignment = blocks.ChoiceBlock(choices=[
+        ('left', 'Слева'),
+        ('center', 'По центру'),
+        ('right', 'Справа')
+    ], default='left')
+
+    class Meta:
+        template = 'blocks/simple_content_block.html'
+
+
+class BlogPage(Page):
+    date = models.DateField("Дата публикации")
+    intro = models.CharField("Введение", max_length=250)
+    body = StreamField([
+        ('content', SimpleContentBlock()),
+        ('text', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
